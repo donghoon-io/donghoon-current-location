786ac269b0cb29544bd978de152214b59d21e483
Donghoon Shin <dhoon@uw.edu>
2025-05-26 23:35:02 -0700

    Update location.js

--- api/location.js
+++ api/location.js
@@ -1,6 +1,10 @@
 let latestCity = 'Seattle';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   if (req.method === 'POST') {
     const { city } = req.body;
     if (!city) return res.status(400).json({ error: 'City is required' });
